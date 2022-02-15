9cbdaa777c0cb43919ab240e1fece0b70941c4d0
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-02-14 21:36:35 -0500

    done UI function, not styling this project

--- main.js
+++ main.js
@@ -4,14 +4,66 @@ const screenLog = document.querySelector(".screen-log");
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SICISSORS = 'scissors';
+const scoreCounters = { win: 0, draw: 0, lose: 0, count: 0 };
+
+const btns = document.querySelectorAll('.action button');
+btns.forEach(btn => btn.addEventListener('click', userChoice));
 
 
-function logString(s = '') {
-    console.log(s);
-
+function logString(s) {
     const p = document.createElement('p');
     p.innerText = s;
     screenLog.appendChild(p);
+}
+
+function userChoice(e) {
+    scoreCounters.count++;
+    const ret = playRound(this.id, computerPlay());
+
+    if (ret.status === 0)
+        scoreCounters.draw++;
+    if (ret.status === 1)
+        scoreCounters.win++;
+    if (ret.status === -1)
+        scoreCounters.lose++;
+
+    updateCounters();
+    logString(ret.msg);
+    checkEndGame();
+}
+
+function updateCounters() {
+    updateStat('win', scoreCounters.win);
+    updateStat('draw', scoreCounters.draw);
+    updateStat('lose', scoreCounters.lose);
+    updateStat('count', scoreCounters.count);
+}
+
+function updateStat(fieldName, newNum) {
+    const field = document.querySelector(`#${fieldName}`);
+    field.value = newNum;
+}
+
+function checkEndGame() {
+    // TODO: looks like alert is not letting the last number update in UI
+    // probably doing too much in event handler, need something better
+    if (scoreCounters.win === 5 || scoreCounters.lose === 5) {
+        alert(`You ${(scoreCounters.win > scoreCounters.lose) ? 'won' : 'lose'}`);
+        reset();
+    }
+}
+
+function reset() {
+    scoreCounters.win = 0;
+    scoreCounters.draw = 0;
+    scoreCounters.lose = 0;
+    scoreCounters.count = 0;
+
+    // clean up the log junk from previous game
+    const list = screenLog.children;
+    const len = list.length;
+    for (let i = len - 1; i > 0; i--)
+        list.item(i).remove();
 }
 
 function getRandomInt(max) {
@@ -21,18 +73,17 @@ function getRandomInt(max) {
 function computerPlay() {
     const listOfOptions = [ROCK, PAPER, SICISSORS];
     let index = getRandomInt(3);
-
     return listOfOptions[index];
 }
 
-// default params are just to get intellisense to work; 
-// i hate non-static typed languages, need to switch to TypeScript
-function playRound(playerSelection = '', computerSelection = '') {
+function playRound(playerSelection, computerSelection) {
     let looser = false;
     playerSelection = playerSelection.toLowerCase();
 
+    // console.log({playerSelection, computerSelection});
+
     if (playerSelection === computerSelection)
-        return { count: 0, msg: `Draw! ${computerSelection}` };
+        return { status: 0, msg: `Draw! ${computerSelection}` };
 
     switch (playerSelection) {
         case ROCK:
@@ -45,35 +96,14 @@ function playRound(playerSelection = '', computerSelection = '') {
             looser = (computerSelection === ROCK)
             break;
         default:
-            return { count: 0, msg: `invalid value: ${playerSelection}` };
+            return { status: 0, msg: `invalid value: ${playerSelection}` };
     }
 
     if (looser)
-        return { count: -1, msg: `You Lose! ${computerSelection} beats ${playerSelection}` };
+        return { status: -1, msg: `You Lose! ${computerSelection} beats ${playerSelection}` };
 
-    return { count: 1, msg: `You Win! ${playerSelection} beats ${computerSelection}` };
+    return { status: 1, msg: `You Win! ${playerSelection} beats ${computerSelection}` };
 }
 
-function game() {
-    let winCount = 0;
-    const loopCount = 5;
-
-    for (let i = 0; i < loopCount; i++) {
-        const userValue = window.prompt('Enter a choice', '');
-        const ret = playRound(userValue, computerPlay())
-        winCount += ret.count
-        logString(ret.msg);
-    }
-
-    logString(`You ${(winCount > 0) ? 'won' : 'suck'}: ${winCount} out of ${loopCount}`);
-}
-
-// logString("Hello, world!")
-
-// for (let i = 0; i < 5; i++)
-//     logString(computerPlay());
-
-// logString(playRound('blah', PAPER));
-
-game()
-
+reset();
+updateCounters();
